3da30b251aa7d2251550f0accb48f6c0a9a4c129
Nitramtj <Nitramtj@b12f80b2-13f7-c241-bba9-584cf6c01a0a>
2011-01-05 22:34:53 +0000

    Now fixed?

---
+++ Brick_Space.cs
@@ -0,0 +1,8 @@
+datablock fxDTSBrickData(brick1x4x3SpaceHatchData)
+{
+	brickFile = "./1x4x3SpaceHatch.blb";
+	category = "Bricks";
+	subCategory = "Misc";
+	uiName = "1x4x3 Space Hatch";
+	iconName = "Add-Ons/Brick_V15/2x2 Corner";
+};
