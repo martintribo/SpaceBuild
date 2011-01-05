cb8c63e4e8139c59d950098e831a12c3ff220c81
Nitramtj <Nitramtj@users.noreply.github.com>
2011-01-05 22:33:11 +0000

    Fixed server.cs conflict.

--- server.cs
+++ server.cs
@@ -4,7 +4,11 @@
 //exec("./Script_Gravity.cs");
 
 //Contains Build Selector tool, which creates vbLists for builds.
+
 exec("./Tool_BuildSelector.cs");
+
+//Space Bricks
+exec("./Brick_Space.cs");
 
 //Cargo player.
 exec("./Player_Cargo.cs");
