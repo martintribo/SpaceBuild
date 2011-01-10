05f6c28acc1eb708f589f06a1b7598fed8744cc9
Nitramtj <Nitramtj@b12f80b2-13f7-c241-bba9-584cf6c01a0a>
2011-01-10 22:41:32 +0000

    Added StationSO, also worked on having modules placed.

--- Script_Modules.cs
+++ Script_Modules.cs
@@ -13,6 +13,9 @@ function ModuleSO::onAdd(%this, %obj)
 {
 	//create the queue for modules
 	%obj.numHatches = 0;
+	%obj.state = "none"; //none state basically means the module isn't represented at all in spaceyou 
+	//%obj.state
+	//%obj.owner`
 }
 
 function ModuleSO::addBrick(%obj, %brick)
@@ -26,4 +29,38 @@ function ModuleSO::addHatch(%obj, %point, %dir1, %dir2)
 	%obj.hatches[%obj.numHatches, "direction"] = %dir1;
 	%obj.vbl.addMarker("Hatch" @ %obj.numHatches, %point, %dir1, %dir2);
 	%obj.numHatches++;
+}
+
+function ModuleSO::attachTo(%obj, %objHatch, %mod, %modHatch)
+{
+	//besides just setting up links and etc, the states and owners of modules must be changed
+	%obj.vbl.markers[%objHatch].alignWith(%mod.vbl.markers[%modHatch]);
+	%obj.vbl.createBricks();
+	%obj.setPosition(%obj.vbl.getCenter());
+	%obj.deploy();
+}
+
+function ModuleSO::deploy(%obj)
+{
+	%obj.setState("Deployed");
+	%obj.vbl.createBricks();
+}
+
+function ModuleSO::setState(%obj, %state)
+{
+	//the position can be calculated differently depending on the state, make sure it stays the same
+	%pos = %obj.getPosition();
+	%obj.setState(%state);
+	%obj.setPosition(%pos);
+}
+
+function ModuleSO::getPosition(%obj)
+{
+	//this eventually needs to change depending on the state
+	return %obj.vbl.getCenter();
+}
+
+function ModuleSO::setPosition(%obj, %pos)
+{
+	%obj.vbl.recenter(%pos);
 }
---
+++ Script_Station.cs
@@ -0,0 +1,25 @@
+function StationSO::onAdd(%this, %obj)
+{
+	//%obj.base
+	
+	%obj.modules = new SimSet();
+}
+
+function StationSO::onRemove(%this, %obj)
+{
+	while (%obj.modules.getCount())
+		%obj.modules.getObject(0).delete();
+	
+	%obj.modules.delete();
+}
+
+function StationSO::addModule(%obj, %mod)
+{
+	if (!isObject(%obj.base))
+	{
+		%obj.base = %mod;
+	}
+	
+	%mod.owner = %obj;
+	%obj.modules.add(%mod);
+}
