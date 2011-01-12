acbdbd6004a22f602b93c62976f6e7ec38accde2
manipulatorsvn <manipulatorsvn@b12f80b2-13f7-c241-bba9-584cf6c01a0a>
2011-01-12 23:35:58 +0000

    Made Tool_SBTool.cs use raycasts.

--- Tool_SBTool.cs
+++ Tool_SBTool.cs
@@ -1,164 +1,11 @@
 //This tool is mostly for debugging, it allows users to interface with the gamemode
 //This will be done through events later
 
-datablock AudioProfile(SBToolExplosionSound)
-{
-   filename    = "./sound/arrowHit.wav";
-   description = AudioClose3d;
-   preload = true;
-};
-
-datablock AudioProfile(SBToolFireSound)
-{
-   filename    = "./sound/bowFire.wav";
-   description = AudioClosest3d;
-   preload = true;
-};
-
-
-//arrow trail
-datablock ParticleData(SBToolTrailParticle)
-{
-	dragCoefficient		= 3.0;
-	windCoefficient		= 0.0;
-	gravityCoefficient	= 0.0;
-	inheritedVelFactor	= 0.0;
-	constantAcceleration	= 0.0;
-	lifetimeMS		= 200;
-	lifetimeVarianceMS	= 0;
-	spinSpeed		= 10.0;
-	spinRandomMin		= -50.0;
-	spinRandomMax		= 50.0;
-	useInvAlpha		= false;
-	animateTexture		= false;
-	//framesPerSec		= 1;
-
-	textureName		= "base/data/particles/dot";
-	//animTexName		= "~/data/particles/dot";
-
-	// Interpolation variables
-	colors[0]	= "1 1 1 0.2";
-	colors[1]	= "1 1 1 0.0";
-	sizes[0]	= 0.2;
-	sizes[1]	= 0.01;
-	times[0]	= 0.0;
-	times[1]	= 1.0;
-};
-
-datablock ParticleEmitterData(SBToolTrailEmitter)
-{
-   ejectionPeriodMS = 2;
-   periodVarianceMS = 0;
-
-   ejectionVelocity = 0; //0.25;
-   velocityVariance = 0; //0.10;
-
-   ejectionOffset = 0;
-
-   thetaMin         = 0.0;
-   thetaMax         = 90.0;  
-
-   particles = SBToolTrailParticle;
-};
-
-//effects
-datablock ParticleData(SBToolExplosionParticle)
-{
-	dragCoefficient      = 5;
-	gravityCoefficient   = 0.1;
-	inheritedVelFactor   = 0.2;
-	constantAcceleration = 0.0;
-	lifetimeMS           = 500;
-	lifetimeVarianceMS   = 300;
-	textureName          = "base/data/particles/chunk";
-	spinSpeed		= 10.0;
-	spinRandomMin		= -50.0;
-	spinRandomMax		= 50.0;
-	colors[0]     = "0.9 0.9 0.6 0.9";
-	colors[1]     = "0.9 0.5 0.6 0.0";
-	sizes[0]      = 0.25;
-	sizes[1]      = 0.0;
-};
-
-datablock ParticleEmitterData(SBToolExplosionEmitter)
-{
-   ejectionPeriodMS = 7;
-   periodVarianceMS = 0;
-   ejectionVelocity = 5;
-   velocityVariance = 1.0;
-   ejectionOffset   = 0.0;
-   thetaMin         = 0;
-   thetaMax         = 90;
-   phiReferenceVel  = 0;
-   phiVariance      = 360;
-   overrideAdvance = false;
-   particles = "SBToolExplosionParticle";
-};
-
-datablock ExplosionData(SBToolExplosion)
-{
-   //explosionShape = "";
-	soundProfile = SBToolExplosionSound;
-
-   lifeTimeMS = 150;
-
-   particleEmitter = SBToolExplosionEmitter;
-   particleDensity = 10;
-   particleRadius = 0.2;
-
-   faceViewer     = true;
-   explosionScale = "1 1 1";
-
-   shakeCamera = false;
-   camShakeFreq = "10.0 11.0 10.0";
-   camShakeAmp = "1.0 1.0 1.0";
-   camShakeDuration = 0.5;
-   camShakeRadius = 10.0;
-
-   // Dynamic light
-   lightStartRadius = 0;
-   lightEndRadius = 2;
-   lightStartColor = "0.3 0.6 0.7";
-   lightEndColor = "0 0 0";
-};
-
-
-//projectile
-datablock ProjectileData(SBToolProjectile)
-{
-   //projectileShapeName = "./shapes/arrow.dts";
-
-   directDamage        = 0;
-   directDamageType    = $DamageType::ArrowDirect;
-
-   radiusDamage        = 0;
-   damageRadius        = 0;
-   radiusDamageType    = $DamageType::ArrowDirect;
-
-   explosion           = SBToolExplosion;
-   //particleEmitter     = SBToolTrailEmitter;
-
-   muzzleVelocity      = 50;
-   velInheritFactor    = 1;
-
-   armingDelay         = 0;
-   lifetime            = 100;
-   fadeDelay           = 70;
-   bounceElasticity    = 0;
-   bounceFriction      = 0;
-   isBallistic         = false;
-   gravityMod = 0.0;
-
-   hasLight    = false;
-   lightRadius = 3.0;
-   lightColor  = "0 0 0.5";
-};
-
 datablock ItemData(SBToolItem : wandItem)
 {
 	uiName = "SB Tool";
 	doColorShift = true;
-	colorShiftColor = "0.1 0.7 0.05 1.000";
+	colorShiftColor = "1 0 1 1";
 	image = SBToolImage;
 };
 
@@ -253,7 +100,25 @@ datablock ShapeBaseImageData(SBToolImage)
 
 };
 
-function SBToolProjectile::onCollision(%this,%obj,%col,%fade,%pos,%normal)
+function SBToolImage::onFire(%this, %obj, %slot)
+{
+	%mouseVec = %obj.getEyeVector();
+	%cameraPoint = %obj.getEyePoint();
+	%selectRange = 5;
+	%mouseScaled = VectorScale(%mouseVec, %selectRange);
+	%rangeEnd = VectorAdd(%cameraPoint, %mouseScaled);
+	%searchMasks = $TypeMasks::FxBrickAlwaysObjectType;
+	%target = ContainerRayCast(%cameraPoint, %rangeEnd, %searchMasks, %client.player);
+	%%col = getWord(%target, 0);
+	
+	if(isObject(%target))
+	{
+		SBToolCollision(%obj, %col);
+	}
+	
+}
+
+function SBToolCollision(%obj, %col)
 {
 	%client = %obj.client;
 	
