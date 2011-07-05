0f04dbd9e26cab6f54d4cd85d135fe8b7123be72
manipulatorsvn <Nitramtj@users.noreply.github.com>
2011-07-05 12:52:52 +0000

    Added moduleBase marker to the grid template (where the module's bottom center goes. Added getSlot() to fxDTSBrick which returns the slot of a brick.

--- Script_MCSlot.cs
+++ Script_MCSlot.cs
@@ -22,3 +22,19 @@ function MCSlotBrickFactory::onCreateBrick(%obj, %brick)
 {
 	%brick.slot = %obj.slot;
 }
+
+function fxDTSBrick::findSlot(%obj)
+{
+	%slot = 0;
+	if (%obj.getNumDownBricks())
+		%slot = %obj.getDownBrick(0).slot;
+	else if (%obj.getNumUpBricks())
+		%slot = %obj.getUpBrick(0).slot;
+	
+	return %slot;
+}
+
+function fxDTSBrick::isInSlot(%obj)
+{
+	
+}
