97ff40842f5912c08d311f86bdbb1544e2ea004d
Aloshi <Aloshi@Aloshi-PC>
2012-01-04 18:32:48 -0600

    Changed Vehicle_SpaceShuttle.cs to stop causing console spam when loading bricks.

--- Vehicle_SpaceShuttle.cs
+++ Vehicle_SpaceShuttle.cs
@@ -188,10 +188,11 @@ package shuttleTrust
 {
 	function getTrustLevel(%obj, %other, %a1, %a2)
 	{
-		if(%obj.getDataBlock() != 0 && %obj.getDataBlock().getName() $= "SpaceShuttleVehicle")
+		//Now uses .dataBlock instead of .getDataBlock() - no more console errors about checking with objects that don't have a datablock.
+		if(%obj.dataBlock != 0 && %obj.getDataBlock().getName() $= "SpaceShuttleVehicle")
 			return 1;
 		
-		if(%other.getDataBlock() != 0 && %other.getDataBlock().getName() $= "SpaceShuttleVehicle")
+		if(%other.dataBlock != 0 && %other.getDataBlock().getName() $= "SpaceShuttleVehicle")
 			return 1;
 		
 		Parent::getTrustLevel(%obj, %other, %a1, %a2);
