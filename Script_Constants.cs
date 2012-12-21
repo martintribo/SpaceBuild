4b3ed8d386344907fd38857c9fe54c2f54957a4e
Nitramtj <nitramtj@gmail.com>
2012-12-21 02:16:41 -0600

    Random modifications for automatic loading of station and gamemode config.

---
+++ Script_Constants.cs
@@ -0,0 +1,10 @@
+//This file will hopefully cut down on difficulty of renaming spacebuild, if that needs to happen.
+
+$Spacebuild::AddOnPath = "Add-Ons/Gamemode_Spacebuild/";
+$Spacebuild::SavePath = "config/server/spacebuild/";
+$Spacebuild::StationFile = "default.sbs";
+$Spacebuild::MCFFile = "default.sbm";
+$Spacebuild::DefaultMCL = "MCL_Grid";
+$Spacebuild::MCFPosition = "0 0 0";
+$Spacebuild::StationPosition = "0 -2000 3000";
+$Spacebuild::startModuleFile = "starterModule.bls";
