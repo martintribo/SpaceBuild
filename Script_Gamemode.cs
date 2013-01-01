74ad0a6a9672ae7260b3869e02d555b4e8a26c54
Nitramtj <Nitramtj@users.noreply.github.com>
2012-12-31 21:53:31 -0600

    Added RTB preference for max saved modules per BLID.

---
+++ Script_Gamemode.cs
@@ -0,0 +1,59 @@
+function setupSpacebuild(%mg)
+{
+	loadRunway();
+	createSpaceObjects(%mg);
+
+}
+
+function createSpaceObjects(%mg)
+{
+	%so = new ScriptObject(MCFacility);
+	%mg.station = new ScriptObject(StationSO);
+	%mg.mcf = new ScriptObject(MCFacility);
+	%mg.mcf.setPosition($Spacebuild::MCFPosition);
+	
+	%mcl = new ScriptObject($Spacebuild::DefaultMCL);
+	%mg.mcf.setMCL(%mcl);
+	
+	%defaultStationPath = $Spacebuild::SavePath @ $Spacebuild::StationFile;
+	%defaultMCFPath = $Spacebuild::SavePath @ $Spacebuild::MCFFile;
+	
+	if (isFile(%defaultStationPath))
+		%mg.station.import(%defaultStationPath);
+	else
+		createDefaultStation(%mg);
+	
+	if (isFile(%defaultMCFPath))
+		%mg.mcf.import(%defaultMCFPath);
+}
+
+//temporary fix
+function loadRunway()
+{
+	%runwayBLS = $Spacebuild::AddOnPath @ $Spacebuild::RunwayFile;
+	%vbl = newVBL();
+	%vbl.loadBLSFile(%runwayBLS);
+	%vbl.createBricks();
+	%vbl.delete();
+}
+
+function createDefaultStation(%mg)
+{
+	%defaultStationPath = $Spacebuild::SavePath @ $Spacebuild::StationFile;
+	
+	%vbl = newVBL();
+	%vbl.loadBLSFile($Spacebuild::AddOnPath @ $Spacebuild::StarterModuleFile);
+	%mg.mcf.scanVBL(%vbl);
+	if (!%verificationError)
+	{
+		%mod = %mg.mcf.popModule();
+		%mod.state = "Deployed"; //Shouldn't have to manually do this
+		error("Mod vbl count: " @ %mod.vbl.getCount());
+		%mod.vbl.shiftBricks($Spacebuild::StationDisplacement);
+		%mod.vbl.createBricks();
+		%mg.station.addModule(%mod);
+		
+		%mg.station.export(%defaultStationPath);
+	}
+	%vbl.delete();
+}
---
+++ server.cs
@@ -0,0 +1,111 @@
+//This will exec all other SpaceBuild-specific files.
+
+//disable required add-on ui elements
+JeepVehicle.uiName = "";
+
+//First execute constants file
+exec("./Script_Constants.cs");
+
+//Space bricks (hatches)
+exec("./Brick_Space.cs");
+
+//Cargo player (not yet used, waste of datablocks)
+//exec("./Player_Cargo.cs");
+
+//Module creation facility script object, generation functions
+exec("./Script_MCF.cs");
+exec("./Script_MCSlot.cs");
+exec("./Script_MCL_Grid.cs");
+
+//Modules script object
+exec("./Script_Modules.cs");
+
+//Station script object
+exec("./Script_Station.cs");
+
+//SBTool, for interfacing with the gamemode
+exec("./Tool_SBTool.cs");
+
+//Space Shuttle (also execs Particle_Shuttle.cs)
+exec("./Vehicle_SpaceShuttle.cs");
+
+//SBTick (currently burning up and station locator)
+exec("./Script_SBTick.cs");
+
+//Contains no-gravity player scripts (should be replaced soon)
+exec("./Script_Gravity.cs");
+
+//Enforces rules for the server (no hammers/wrenches/wands/building in space)
+exec("./Script_Rules.cs");
+
+//Events for interacting with the MCF
+exec("./Event_Space.cs");
+
+//Contains helper functions for testing out specific parts of the gamemode
+exec("./Script_Tests.cs");
+
+//SpaceBuild player, that can easily navigate in a zero-gravity environment (right click)
+exec("./Player_Space.cs");
+
+//Core gamemode scripts (loading up initial station, etc)
+exec("./Script_Gamemode.cs");
+
+//RTB Registration
+if(isFile("Add-Ons/System_ReturnToBlockland/server.cs"))
+{
+	if(!$RTB::RTBR_ServerControl_Hook)
+		exec("Add-Ons/System_ReturnToBlockland/RTBR_ServerControl_Hook.cs");
+	
+	RTB_registerPref("Max Module Saves", $Spacebuild::Name, "$Spacebuild::Prefs::MaxModuleSaves", "int 0 9999", $Spacebuild::AddOnFolderName, 30, 0, 1);
+}
+else
+{
+	$Spacebuild::Prefs::MaxModuleSaves = 30;
+}
+
+
+
+	
+
+package SpacebuildGamemode
+{
+	function MiniGameSO::onAdd(%obj)
+	{
+		Parent::onAdd(%obj);
+		
+		if (%obj.owner != 0)
+			return;
+			
+		//The gamemode system cannot load the hammer, so we must do it ourselves
+		%obj.StartEquip[0] = HammerItem.getId();
+		
+		setupSpacebuild(%obj);
+	}
+	
+	function setSkyBox(%sky)
+	{
+		%sky = "Add-Ons/Gamemode_Spacebuild/sky/space.dml";
+		Parent::setSkyBox(%sky);
+	}
+	
+	//Normally making the ground transparent seems to make the bottom texture render, but we want it to anyways in this case
+	function EnvGuiServer::SetAdvancedMode()
+	{
+		Parent::SetAdvancedMode();
+		
+		Sky.noRenderBans = 1;
+		Sky.renderBottomTexture = 1;
+		
+		Sky.setScale("1 1 1");
+	}
+	
+	function MiniGameSO::addMember(%obj, %client)
+	{	
+		parent::addMember( %obj, %client );
+		
+		// kind of a hack, but some things are delayed when the player is added
+		%client.schedule(150, "giveModuleSlot");
+	}
+};
+
+activatePackage(SpacebuildGamemode);
