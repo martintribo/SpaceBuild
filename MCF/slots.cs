90337feefe8b8aa82f79a13df361fab8adbdaeba
Nitramtj <Nitramtj@users.noreply.github.com>
2013-06-28 19:31:35 -0500

    Support for adding all slot objects, and position/rotating.

--- MCF/slots.cs
+++ MCF/slots.cs
@@ -2,6 +2,9 @@
 
 function SlotSO::onAdd(%this, %obj)
 {
+	%obj.slotObjects = new SimSet();
+	%obj.slotObjNum = 0;
+	
 	%obj.storageSets = new SimSet();
 	%obj.storages = new SimSet();
 	%obj.storageNum = 0;
@@ -9,23 +12,195 @@ function SlotSO::onAdd(%this, %obj)
 	%obj.structureSets = new SimSet();
 	%obj.structures = new SimSet();
 	%obj.structureNum = 0;
+	
+	//%obj.position
+	//%obj.angleId
 }
 
 function SlotSO::onRemove(%this, %obj)
 {
-	while (%obj.storages.getCount())
-		%obj.storages.getObject(0).delete();
+	while (%obj.slotObjects.getCount())
+		%obj.slotObjects.getObject(0).delete();
+	%obj.slotObjects.delete();
+	
 	while (%obj.storageSets.getCount())
 		%obj.storageSets.getObject(0).delete();
 	%obj.storageSets.delete();
 	%obj.storages.delete();
 	
-	while (%obj.structures.getCount())
-		%obj.structures.getObject(0).delete();
 	while (%obj.structureSets.getCount())
 		%obj.structureSets.getObject(0).delete();
 	%obj.structureSets.delete();
 	%obj.structures.delete();
+}
+
+function SlotSO::isRendering(%obj)
+{
+	return %obj.rendering;
+}
+
+function SlotSO::setRendering(%obj, %value)
+{
+	if (!%obj.rendering && %value)
+	{
+		%obj.rendering = true;
+		
+		for (%i = 0; %i < %obj.slotObjects.getCount(); %i++)
+			%obj.slotObjects.getObject(%i).setRendering(true);
+	}
+	else if (%obj.rendering && !%value)
+	{
+		%obj.rendering = false;
+		
+		for (%i = 0; %i < %obj.slotObjects.getCount(); %i++)
+			%obj.slotObjects.getObject(%i).setRendering(false);
+	}
+}
+
+function SlotSO::getPosition(%obj)
+{
+	return %obj.position;
+}
+
+function SlotSO::setPosition(%obj, %newPos)
+{
+	%wasRendering = %obj.isRendering();
+	%obj.setRendering(false);
+	
+	%oldPos = %obj.getPosition();
+	
+	for (%i = 0; %i < %obj.slotObjects.getCount(); %i++)
+	{
+		%slotObj = %obj.slotObjects.getObject(%i);
+		
+		%dis = VectorSub(%slotObj.getPosition(), %oldPos);
+		%slotObjPos = VectorAdd(%dis, %newPos);
+		
+		%slotObj.setPosition(%slotObjPos);
+	}
+	
+	%obj.position = %newPos;
+	
+	if (%wasRendering)
+		%obj.setRendering(true);
+}
+
+function SlotSO::getAngleId(%obj)
+{
+	return %obj.angleId;
+}
+
+function SlotSO::setAngleId(%obj, %angleId)
+{
+	//first normalize the angleId
+	if (%angleId < 0)
+		%angleId = 4 - (mAbs(%angleId) % 4);
+	else
+		%angleId %= 4;
+	
+	%curAng = %obj.getAngleId();
+	
+	if (%curAng != %angleId)
+	{
+		%wasRendering = %obj.isRendering();
+		%obj.setRendering(false);
+		
+		if (%curAng > %angleId)
+			%curAng -= 4;
+		
+		%rotAng = %angleId - %curAng;
+		%center = %slotObj.getPosition(); // this isn't necessarily the center
+		
+		for (%i = 0; %i < %obj.slotObjects.getCount(); %i++)
+		{
+			%slotObj = %obj.slotObjects.getObject(%i);
+			%oldDis = VectorSub(%slotObj.getPosition(), %oldPos);
+			switch (%rotAng)
+			{
+				case 1:
+					%newDis = getWord(%oldDis, 1) SPC -getWord(%oldDis, 0) SPC getWord(%oldDis, 2);
+				case 2:
+					%newDis = -getWord(%oldDis, 1) SPC -getWord(%oldDis, 0) SPC getWord(%oldDis, 2);
+				case 3:
+					%newDis = -getWord(%oldDis, 1) SPC getWord(%oldDis, 0) SPC getWord(%oldDis, 2);
+			}
+			
+			%newPos = VectorAdd(%newDis, %center);
+			%slotObj.setAngleId(%angleId);
+			%slotObj.setPosition(%newPos);
+		}
+		
+		if (%wasRendering)
+			%obj.setRendering(true);
+	}
+}
+
+//General SlotObject Code
+
+function SlotSO::setSlotObjectName(%obj, %slotObj, %name)
+{
+	if (%slotObj.slotName !$= "")
+		%obj.slotObjects[%slotObj.slotName] = "";
+	
+	%obj.slotObjects[%name] = %slotObj;
+	%slotObj.slotName = %name;
+}
+
+function SlotSO::addSlotObject(%obj, %slotObj, %name)
+{
+	%slotObj.slot = %obj;
+	%obj.slotObjects.add(%slotObj);
+	
+	if (%obj.isRendering())
+		%slotObj.setRendering(true);
+	else
+		%obj.setRendering(false);
+	
+	if (isClass(%slotObj, "ModuleStorage"))
+		%obj.onAddStorage(%slotObj, %name);
+	else if (isClass(%slotObj, "ModuleStructure"))
+		%obj.onAddStructure(%slotObj, %name);
+	else
+		%obj.onAddBasicSlotObject(%slotObj, %name);
+}
+
+function SlotSO::removeSlotObject(%obj, %slotObj)
+{
+	%obj.slotObjects[%slotObj.slotName] = "";
+	%slotObj.slotName = "";
+	%slotObj.slot = "";
+	
+	if (isClass(%slotObj, "ModuleStorage"))
+		%obj.onRemoveStorage(%slotObj, %name);
+	else if (isClass(%slotObj, "ModuleStructure"))
+		%obj.onRemoveStructure(%slotObj, %name);
+	
+	//Not needed yet
+	//else
+	//	%obj.onRemoveBasicSlotObject(%slotObj, %name);
+}
+
+function SlotSO::onAddBasicSlotObject(%obj, %slotObj, %name)
+{
+	if (%name $= "")
+	{
+		%name = "SlotObject" @ %obj.slotObjNum;
+		%obj.slotObjNum++;
+		while (isObject(%obj.slotObjects[%name]))
+		{
+			%name = "SlotObject" @ %obj.slotObjNum;
+			%obj.slotObjNum++;
+		}
+	}
+	%obj.setSlotObjectName(%storage, %name);
+}
+
+function SlotSO::getSlotObjectByName(%obj, %name)
+{
+	if (isObject(%obj.slotObjects[%name]))
+		return %obj.slotObjects[%name];
+	else
+		return 0;
 }
 
 //Storage Related Code
@@ -51,7 +226,7 @@ function SlotSO::getStorageSet(%obj, %type)
 	return %obj.storageSets[%name];
 }
 
-function SlotSO::addStorage(%obj, %storage, %name)
+function SlotSO::onAddStorage(%obj, %storage, %name)
 {
 	%type = %storage.getModuleType();
 
@@ -64,18 +239,16 @@ function SlotSO::addStorage(%obj, %storage, %name)
 	{
 		%name = "Storage" @ %obj.storageNum;
 		%obj.storageNum++;
-		while (isObject(%obj.storages[%name]))
+		while (isObject(%obj.slotObjects[%name]))
 		{
 			%name = "Storage" @ %obj.storageNum;
 			%obj.storageNum++;
 		}
 	}
-	%obj.setStorageName(%storage, %name);
-	
-	%storage.slot = %obj;
+	%obj.setSlotObjectName(%storage, %name);
 }
 
-function SlotSO::removeStorage(%obj, %storage)
+function SlotSO::onRemoveStorage(%obj, %storage)
 {
 	%type = %storage.getModuleType();
 	
@@ -83,11 +256,6 @@ function SlotSO::removeStorage(%obj, %storage)
 	
 	%set.remove(%storage);
 	%obj.storages.remove(%storage);
-	
-	%obj.storages[%storage.slotName] = "";
-	%storage.slotName = "";
-	
-	%storage.slot = "";
 }
 
 function SlotSO::getNumStorages(%obj, %type)
@@ -110,122 +278,14 @@ function SlotSO::getStorage(%obj, %i, %type)
 	return %set.getObject(%i);
 }
 
-function SlotSO::getStorageByName(%obj, %name)
-{
-	%storage = %obj.storages[%name];
-	
-	if (!isObject(%storage))
-		%storage = 0;
-	
-	return %storage;
-}
-
-function SlotSO::setStorageName(%obj, %storage, %name)
-{
-	if (%storage.slotName !$= "")
-		%obj.storages[%storage.slotName] = "";
-	
-	%storage.slotName = %name;
-	%obj.storages[%name] = %storage;
-}
-
 //Structure Code
 
-function SlotSO::addStructureSet(%obj, %type)
+function isClass(%obj, %type)
 {
-	%name = %type.getName();
-	%set = new SimSet();
-	%obj.structureSets.add(%set);
-	%obj.structureSets[%name] = %set;
-	%set.name = %name;
-	
-	return %set;
-}
-
-function SlotSO::getStructureSet(%obj, %type)
-{
-	%name = %type.getName();
-	
-	if (!isObject(%obj.structureSets[%name]))
-		%obj.addStructureSet(%type);
-	
-	return %obj.structureSets[%name];
-}
-
-function SlotSO::addStructure(%obj, %structure, %name)
-{
-	%type = %structure.getModuleType();
-
-	%set = %obj.getStructureSet(%type);
-	
-	%set.add(%structure);
-	%obj.structures.add(%structure);
-	
-	if (%name $= "")
-	{
-		%name = "Structure" @ %obj.structureNum;
-		%obj.structureNum++;
-		while (isObject(%obj.structures[%name]))
-		{
-			%name = "Structure" @ %obj.structureNum;
-			%obj.structureNum++;
-		}
-	}
-	%obj.setStructureName(%structure, %name);
-	
-	%structure.slot = %obj;
-}
-
-function SlotSO::removeStructure(%obj, %structure)
-{
-	%type = %structure.getModuleType();
-	
-	%set = %obj.getStructureSet(%type);
-	
-	%set.remove(%structure);
-	%obj.structures.remove(%structure);
-	
-	%obj.structures[%structure.slotName] = "";
-	%structure.slotName = "";
-	
-	%structure.slot = "";
-}
-
-function SlotSO::getNumStructures(%obj, %type)
-{
-	if (%type !$= "")
-		%set = %obj.getStructureSet(%type);
+	if (%obj.getClassName() $= %type)
+		return true;
+	else if (%obj.getClassName() $= "ScriptObject" && (%obj.class $= %type || %obj.superClass $= %type || %obj.getName() $= %type))
+		return true;
 	else
-		%set = %obj.structures;
-	
-	return %set.getCount();
-}
-
-function SlotSO::getStructure(%obj, %i, %type)
-{
-	if (%type !$= "")
-		%set = %obj.getStructureSet(%type);
-	else
-		%set = %obj.structures;
-	
-	return %set.getObject(%i);
-}
-
-function SlotSO::getStructureByName(%obj, %name)
-{
-	%structure = %obj.structures[%name];
-	
-	if (!isObject(%structure))
-		%structure = 0;
-	
-	return %structure;
-}
-
-function SlotSO::setStructureName(%obj, %structure, %name)
-{
-	if (%structure.slotName !$= "")
-		%obj.structures[%structure.slotName] = "";
-	
-	%structure.slotName = %name;
-	%obj.structures[%name] = %structure;
+		return false;
 }
